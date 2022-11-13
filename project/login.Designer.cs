38a4086110a6f92b1d98479b3cfbfeae6bc4a060
Theeraporn <63072072+heroclik@users.noreply.github.com>
2022-11-13 17:53:33 +0700

    Add files via upload

---
+++ project/login.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace project
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usernametbox = new System.Windows.Forms.TextBox();
+            this.passwordtbox = new System.Windows.Forms.TextBox();
+            this.loginbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(392, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(392, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // usernametbox
+            // 
+            this.usernametbox.Location = new System.Drawing.Point(510, 110);
+            this.usernametbox.Name = "usernametbox";
+            this.usernametbox.Size = new System.Drawing.Size(133, 20);
+            this.usernametbox.TabIndex = 2;
+            // 
+            // passwordtbox
+            // 
+            this.passwordtbox.Location = new System.Drawing.Point(510, 169);
+            this.passwordtbox.Name = "passwordtbox";
+            this.passwordtbox.Size = new System.Drawing.Size(133, 20);
+            this.passwordtbox.TabIndex = 3;
+            // 
+            // loginbtn
+            // 
+            this.loginbtn.Location = new System.Drawing.Point(457, 236);
+            this.loginbtn.Name = "loginbtn";
+            this.loginbtn.Size = new System.Drawing.Size(75, 23);
+            this.loginbtn.TabIndex = 4;
+            this.loginbtn.Text = "button1";
+            this.loginbtn.UseVisualStyleBackColor = true;
+            this.loginbtn.Click += new System.EventHandler(this.loginbtn_Click);
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.loginbtn);
+            this.Controls.Add(this.passwordtbox);
+            this.Controls.Add(this.usernametbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "login";
+            this.Text = "login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usernametbox;
+        private System.Windows.Forms.TextBox passwordtbox;
+        private System.Windows.Forms.Button loginbtn;
+    }
+}
---
+++ project/login.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;//////////*********
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        public static string cn = "Data Source=.; initial Catalog=book; Trusted_Connection=True";
+
+        private void loginbtn_Click(object sender, EventArgs e)
+        {
+            string sql = string.Format("SELECT * FROM Users WHERE Username = '{0}' AND Password = '{1}'", usernametbox.Text, passwordtbox.Text);
+            SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                Menu m = new Menu();
+                string name = dt.Rows[0]["Username"].ToString();
+                MessageBox.Show("ยินดีต้อนรับคุณ" + name);
+                string role = dt.Rows[0]["Role"].ToString();
+                this.Close();
+            }
+            else
+                MessageBox.Show("Username หรือ Password ไม่ถูกต้อง", "Error");
+        }
+    }
+}
